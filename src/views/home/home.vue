0c3d412d17a8638f4d952600c271180cf869d07f
lin0learning <guojlinn@163.com>
2022-08-27 11:36:28 +0800

    设置home Page 主页keep-alive,返回时保留浏览位置

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div class="home" ref="homeRef">
     <home-banner/>
     <home-search-box  />
     <home-categories />
@@ -11,8 +11,12 @@
   
 </template>
 
+<script>
+  export default { name: "home" }
+</script>
+
 <script setup>
-import { watch, computed } from 'vue'
+import { watch, computed, ref, onActivated } from 'vue'
 import homeBanner from './cpns/home-banner.vue'
 import homeSearchBox from './cpns/home-search-box.vue';
 import homeCategories from './cpns/home-categories.vue';
@@ -27,15 +31,10 @@ homeStore.fetchHotSuggestData()
 homeStore.fetchCategories()
 homeStore.fetchHomeRoomsData()
 //
-const { isReachBottom, scrollTop } = useScroll()
+const homeRef = ref()
+const { isReachBottom, scrollTop } = useScroll(homeRef)
 watch(isReachBottom, (newVal) => {
   if (newVal) {
-    // 设置定时器节流网络请求
-    // setTimeout(() => {
-    //   homeStore.fetchHomeRoomsData().then(() => {
-    //     isReachBottom.value = false
-    //   })
-    // }, 200);
     homeStore.fetchHomeRoomsData().then(() => {
       isReachBottom.value = false
     })
@@ -46,11 +45,22 @@ watch(isReachBottom, (newVal) => {
 const isShowSearchBar = computed(() => {
   return scrollTop.value >= 360
 })
+
+// 跳转home主页时，保留上次滚动位置
+onActivated(() => {
+  homeRef.value?.scrollTo({
+    top: scrollTop.value
+  })
+})
 </script>
 
 <style scoped lang="less">
   .home {
+    height: 100vh;
+    box-sizing: border-box;
+    overflow-y: auto;
     margin-bottom: 50px;
+    
 
     .search-bar {
       position: fixed;
