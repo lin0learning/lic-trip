11c1db232a76645615e1516065644b2b2572c912
lin0learning <guojlinn@163.com>
2022-11-04 11:11:40 +0800

    pref: register pxtoviewport plugin in vite  and remove postcss.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite';
 import { VantResolver } from 'unplugin-vue-components/resolvers';
+import postcsspxtoviewport from 'postcss-px-to-viewport'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,6 +15,16 @@ export default defineConfig({
       resolvers: [VantResolver()],
     }),
   ],
+  css: {
+    postcss: {
+      plugins: [
+        postcsspxtoviewport({
+          unitToConvert: 'px',
+          viewportWidth: 375,
+        })
+      ]
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
